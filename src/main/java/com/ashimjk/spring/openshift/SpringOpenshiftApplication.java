bb73cda3c6a839100e20fdc7fe04aaa2d0ae0cf0
Ashim Khadka <ashim.jung.khadka@gmail.com>
2023-04-26 13:33:05 -0500

    Initial Commit

---
+++ src/main/java/com/ashimjk/spring/openshift/SpringOpenshiftApplication.java
@@ -0,0 +1,13 @@
+package com.ashimjk.spring.openshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOpenshiftApplication.class, args);
+	}
+
+}
